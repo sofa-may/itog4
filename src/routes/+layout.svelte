611957fc089ed76ddc1bd796a2c4c28edd8423f5
Sofa <maysova.sofa08@mail.ru>
2023-06-02 10:35:34 +0300

    gggggggggggg

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,47 @@
+<script>
+    const nav = [
+        {
+            title: "Обо мне",
+            href: "/about",
+            isShow: true,
+        },
+        {
+            title: "Блог",
+            href: "/blog",
+            isShow: true,
+        },
+        {
+            title: "Портфолио",
+            href: "/project",
+            isShow: true,
+        },
+        {
+            title: "Контакты",
+            href: "/contact",
+            isShow: true,
+        },
+    ];
+</script>
+
+<nav>
+    <div class="nav-container">
+        <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
+        <div class="nav-links">
+            {#each nav as link}
+                <a href={link.href} class="link">{link.title}</a>
+            {/each}
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+    <!-- Pages will be injected below -->
+    <slot />
+</div>
+
+<style>
+    .container {
+        max-width: 1400px;
+        margin: 50px auto;
+    }
+</style>
